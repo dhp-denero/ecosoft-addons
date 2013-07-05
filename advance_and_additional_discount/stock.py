d7ab4cfe5d5a6ec750e54993e908471bdfa66aed
kittiu <Unknown>
2013-07-05 18:01:05 +0700

    Minor review

--- advance_and_additional_discount/stock.py
+++ advance_and_additional_discount/stock.py
@@ -13,16 +13,18 @@ class stock_picking(osv.osv):
         res = super(stock_picking, self).action_invoice_create(cr, uid, ids, journal_id,
                                                                 group, type, context=context)
         # Loop through each id (DO), getting its SO/PO's Additional Discount, Write it to Invoice
-        order_obj = type == 'out_invoice' and self.pool.get('sale.order') or self.pool.get('purchase.order')
+        model = type in ('out_invoice', 'out_refund') and 'sale.order' or 'purchase.order'
         inv_obj = self.pool.get('account.invoice')
         pickings = self.browse(cr, uid, ids)
         for picking in pickings:
             add_disc = 0.0
             invoice_id = res[picking.id]
-            order_ids = order_obj.search(cr, uid, [('name','=',picking.origin or '')])
-            if order_ids:
-                results = order_obj.read(cr, uid, order_ids, ['add_disc'])
-                add_disc = results and results[0] and results[0]['add_disc'] or 0.0
+            if model == 'sale.order':
+                orders = inv_obj.browse(cr, uid, invoice_id).sale_order_ids
+            else:
+                orders = inv_obj.browse(cr, uid, invoice_id).purchase_order_ids
+            if orders:
+                add_disc = orders[0] and orders[0].add_disc or 0.0
                 inv_obj.write(cr, uid, [invoice_id], {'add_disc': add_disc}, context)
                 inv_obj.button_compute(cr, uid, [invoice_id])
         return res
