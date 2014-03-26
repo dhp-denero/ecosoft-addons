9d82be14d1e358c0dfebe2c2f00faca820d3be30
kittiu <Unknown>
2014-03-26 20:40:21 +0700

    -

--- mrp_quick_bom/mrp.py
+++ mrp_quick_bom/mrp.py
@@ -22,11 +22,13 @@
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
 from openerp import netsvc
+from collections import Counter
+
 
 class mrp_bom(osv.osv):
-    
+
     _inherit = "mrp.bom"
-    
+
     def action_product_bom_create(self, cr, uid, product_ids, data, context=None):
         if context == None:
             context = {}
@@ -34,13 +36,13 @@ class mrp_bom(osv.osv):
         product_obj = self.pool.get('product.product')
         # Create product
         product_id = product_obj.create(cr, uid, {
-                'name': data['product_name'],          
-                'categ_id': data['product_categ_id'][0],    
-                'uom_id': data['product_uom_id'][0],     
-                'uom_po_id': data['product_uom_id'][0],      
-                'type': data['type'],          
-                'procure_method': data['procure_method'],          
-                'supply_method': data['supply_method'],          
+                'name': data['product_name'],
+                'categ_id': data['product_categ_id'][0],
+                'uom_id': data['product_uom_id'][0],
+                'uom_po_id': data['product_uom_id'][0],
+                'type': data['type'],
+                'procure_method': data['procure_method'],
+                'supply_method': data['supply_method'],
                 'valuation': data['valuation'],
                 'sale_ok': False,
                 'purchase_ok': False,
@@ -48,25 +50,28 @@ class mrp_bom(osv.osv):
         # Create BOM
         product = product_obj.browse(cr, uid, product_id)
         bom_id = self.create(cr, uid, {
-                'product_id': product_id,       
-                'name': data['product_name'],   
+                'product_id': product_id,
+                'name': data['product_name'],
                 'product_qty': 1.0,
                 'product_uom': data['product_uom_id'][0],
                 'type': 'normal',
             })
-        
+
         # Create BOM Line
+        created_product_ids = []
         for product in product_obj.browse(cr, uid, product_ids):
+            created_product_ids.append(product.id)
+            i = Counter(created_product_ids)[product.id]  # To count number of the same product_id
             self.create(cr, uid, {
                     'bom_id': bom_id,
-                    'product_id': product.id,       
+                    'product_id': product.id,
                     'name': product.name,
-                    'product_qty': products and products[product.id] and products[product.id]['product_qty'] or 1.0,
-                    'product_uom': products and products[product.id] and products[product.id]['product_uom'] or product.uom_id.id,
+                    'product_qty': products and products[product.id][i] and products[product.id][i]['product_qty'] or 1.0,
+                    'product_uom': products and products[product.id][i] and products[product.id][i]['product_uom'] or product.uom_id.id,
                     'type': 'normal',
                 })
         return product_id, bom_id
-    
+
     # Create from Order Line
     def action_product_bom_create_from_order_line(self, cr, uid, order_ids, data, context=None):
         if context == None:
@@ -76,14 +81,22 @@ class mrp_bom(osv.osv):
         order = self.pool.get('sale.order').browse(cr, uid, order_ids[0])
         ids = []
         products = {}
+        i = {}
         for line in order.order_line:
             if line.product_id:
-                ids.append(line.product_id.id)
-                products[line.product_id.id] = {
-                                        'name': line.product_id.name,
-                                        'product_qty': line.product_uom_qty,
-                                        'product_uom': line.product_uom.id,
-                                        }
+                product_id = line.product_id.id
+                prod = {'name': line.product_id.name,
+                    'product_qty': line.product_uom_qty,
+                    'product_uom': line.product_uom.id,
+                    }
+                if product_id in ids:
+                    i[product_id] += 1  # Number of times this product is used, as it is possible to have > 1 line with same product
+                    products[product_id].update({i[product_id]: prod})
+                else:
+                    i[product_id] = 1  # First time, create new dict
+                    products[product_id] = {i[product_id]: prod}
+                ids.append(product_id)
+
         if len(ids) == 0:
             raise osv.except_osv(_('Error!'), _('No Product Lines!'))
         ctx = context.copy()
