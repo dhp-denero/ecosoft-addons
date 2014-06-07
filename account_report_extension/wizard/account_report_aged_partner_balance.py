f677619778501c316452c3dfe5b569259e477c55
Kitti U. <kittiu@gmail.com>
2014-06-07 15:19:05 +0700

    Fix Aged Partner Balance report (due date not correct)

--- account_report_extension/wizard/account_report_aged_partner_balance.py
+++ account_report_extension/wizard/account_report_aged_partner_balance.py
@@ -20,6 +20,8 @@
 ##############################################################################
 
 from openerp.osv import osv
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
 class account_aged_trial_balance(osv.osv_memory):
@@ -28,11 +30,53 @@ class account_aged_trial_balance(osv.osv_memory):
     _name = 'account.aged.trial.balance'
 
     def _print_report(self, cr, uid, ids, data, context=None):
-        res = super(account_aged_trial_balance, self)._print_report(cr, uid, ids, data, context)
+        res = {}
+        if context is None:
+            context = {}
+ 
+        data = self.pre_print_report(cr, uid, ids, data, context=context)
+        data['form'].update(self.read(cr, uid, ids, ['period_length', 'direction_selection'])[0])
+ 
+        period_length = data['form']['period_length']
+        if period_length<=0:
+            raise osv.except_osv(_('User Error!'), _('You must set a period length greater than 0.'))
+        if not data['form']['date_from']:
+            raise osv.except_osv(_('User Error!'), _('You must set a start date.'))
+ 
+        start = datetime.strptime(data['form']['date_from'], "%Y-%m-%d")
+ 
+        if data['form']['direction_selection'] == 'past':
+            start = start - relativedelta(days=1)  # kittiu
+            for i in range(5)[::-1]:
+                stop = start - relativedelta(days=period_length-1) # kittiu: added - 1
+                res[str(i)] = {
+                    'name': (i!=0 and (str((5-(i+1)) * period_length) + '-' + str((5-i) * period_length)) or ('+'+str(4 * period_length))),
+                    'stop': start.strftime('%Y-%m-%d'),
+                    'start': (i!=0 and stop.strftime('%Y-%m-%d') or False),
+                }
+                start = stop - relativedelta(days=1)
+        else:
+            start = start + relativedelta(days=1)  # kittiu
+            for i in range(5):
+                stop = start + relativedelta(days=period_length-1)  # kittiu: added - 1
+                res[str(5-(i+1))] = {
+                    'name': (i!=4 and str((i) * period_length)+'-' + str((i+1) * period_length) or ('+'+str(4 * period_length))),
+                    'start': start.strftime('%Y-%m-%d'),
+                    'stop': (i!=4 and stop.strftime('%Y-%m-%d') or False),
+                }
+                start = stop + relativedelta(days=1)
+        data['form'].update(res)
+        if data.get('form',False):
+            data['ids']=[data['form'].get('chart_account_id',False)]
+        # kittiu
         data['form'].update(self.read(cr, uid, ids, ['partner_ids'], context=context)[0])
         data['model'] = 'res.partner'
-        res.update({'report_name': 'account.aged_trial_balance_ext'}),
-        return res
+        # --
+        return {
+            'type': 'ir.actions.report.xml',
+            'report_name': 'account.aged_trial_balance_ext',
+            'datas': data
+        }
 
 account_aged_trial_balance()
 
